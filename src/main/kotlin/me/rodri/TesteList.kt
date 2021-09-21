7df8a46610d671ae4409e939a298b75cbbe86102
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-09-21 11:11:43 -0300

    Aula 08 Organizando a lista com sortedBy

--- src/main/kotlin/me/rodri/TesteList.kt
+++ src/main/kotlin/me/rodri/TesteList.kt
@@ -1,8 +1,8 @@
 package me.rodri
 
 fun main() {
-    val joao = funcionario("João", 1000.0)
-    val pedro = funcionario("Pedro", 2000.0)
+    val joao = funcionario("João", 2000.0)
+    val pedro = funcionario("Pedro", 1500.0)
     val maria = funcionario("Maria", 4000.0)
 
     val funcionarios = listOf(joao, pedro, maria)
@@ -13,6 +13,13 @@ fun main() {
     println("______________________________")
     println(funcionarios.find {
         it.nome == "Maria" })
+
+    println("______________________________")
+    funcionarios.
+    sortedBy {
+        it.salario}.
+    forEach{
+        println(it)}
 }
 data class funcionario(val nome: String, val salario: Double){
     override fun toString(): String =
